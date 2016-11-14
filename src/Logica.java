7491ac47664a983f0361fa30c1120e883e2737ff
Santiago <Santiago@M>
2016-11-13 23:48:24 -0500

    Logica

---
+++ src/Logica.java
@@ -0,0 +1,13 @@
+import processing.core.PApplet;
+
+public class Logica {
+	
+	private PApplet app;
+	
+	public Logica(PApplet app) {
+		this.app=app;
+		
+		
+	}
+
+}
--- src/MainProcras.java
+++ src/MainProcras.java
@@ -1,18 +1,22 @@
 import processing.core.PApplet;
 
 public class MainProcras extends PApplet {
+	
+	Logica log;
+	
 	public static void main(String[] args) {
 		PApplet.main("MainProcras");
 	}
 
 	@Override
 	public void settings() {
-		size(1024,600);
-		//prueba
+		size(650,1024);
+		
 	}
 
 	@Override
 	public void setup() {
+		log = new Logica(this);
 
 	}
 
